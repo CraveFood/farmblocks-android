37336d09d8b40c1f16ab0710b8ad329593a3f73c
KennedyDias <kennedysouzadias@gmail.com>
2019-08-16 12:48:07 -0300

    Improve text styles screen

--- app/src/main/java/br/com/farmblocks_android/TextStylesActivity.kt
+++ app/src/main/java/br/com/farmblocks_android/TextStylesActivity.kt
@@ -1,6 +1,7 @@
 package br.com.farmblocks_android
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class TextStylesActivity : AppCompatActivity() {
@@ -9,6 +10,12 @@ class TextStylesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_styles)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) onBackPressed()
+        return super.onOptionsItemSelected(item)
     }
 
 }
